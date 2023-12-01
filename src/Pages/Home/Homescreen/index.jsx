d2c16ad883e7dd982f32eea84fb1311abd3b9b1d
GiRebizzi22 <giovannamiguelrebizz@gmail.com>
2023-12-01 13:46:30 -0300

    ggg

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,20 @@
 import HeroSection from '../HeroSection';
-
+import MySkills from '../MySkills';
+import AboutMe from './AboutMe';
+import MyPortfolio from '../MyPortfolio';
+import Testimonials from '../Testimonials';
+import ContactMe from './ContactMe';
+import Footer from '../../Footer';
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <MySkills />
+      <MyPortfolio />
+      <AboutMe />
+      {/* <Testimonials /> */}
+      <ContactMe />
+      <Footer />
     </>
   );
 }
